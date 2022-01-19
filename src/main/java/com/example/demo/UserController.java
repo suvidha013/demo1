a4aebf2336104e5048cb092de19ca7aff3442963
suvidha013 <tcstrainingspringboot@gmail.com>
2022-01-19 18:15:49 +0530

    added delete method

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 
@@ -18,9 +18,16 @@ String getUser(){
 		return "testing";
 	
 }
+	@PostMapping("/user")
 	void createUser(@RequestBody User user )
 	 {
 		 System.out.println(user.getName());
 	 }
+	@DeleteMapping("/user")
+	void deleteUser()
+	{
+		System.out.println("hi");
+		
+	}
 	
 	}
